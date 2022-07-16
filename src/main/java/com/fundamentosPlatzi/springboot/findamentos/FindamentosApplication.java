df9ac7e81e1bf4d97ce488577a1b944337c01752
jparagon23 <jparagon2021@gmail.com>
2022-07-16 13:33:20 -0500

    Primer mensaje

---
+++ src/main/java/com/fundamentosPlatzi/springboot/findamentos/FindamentosApplication.java
@@ -0,0 +1,13 @@
+package com.fundamentosPlatzi.springboot.findamentos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FindamentosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FindamentosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fundamentosPlatzi/springboot/findamentos/FindamentosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fundamentosPlatzi.springboot.findamentos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FindamentosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
